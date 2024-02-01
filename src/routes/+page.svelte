0acb6f594da8407805cc1574d62244f60672174e
xdnw <jessepaleg@outlook.com>
2024-02-01 12:10:18 +0000

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,14 +4,9 @@ import Navbar from '../components/Navbar.svelte'
 import Sidebar from '../components/Sidebar.svelte'
 import Footer from '../components/Footer.svelte'
 import { onMount } from 'svelte';
-// let count = 0;
-
-// console.log("Hello World");
 
 // let url = "https://locutus.s3.ap-southeast-2.amazonaws.com/test";
-
 // let data;
-
 // onMount(async () => {
 //   try {
 //     const response = await fetch('https://locutus.s3.ap-southeast-2.amazonaws.com/test');
@@ -21,6 +16,7 @@ import { onMount } from 'svelte';
 //     console.error('Error reading from S3 bucket:', error);
 //   }
 // });
+
 </script>
 <Navbar />
 <Sidebar />
