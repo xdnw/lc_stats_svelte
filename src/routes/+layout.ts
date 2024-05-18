91fdd2a31f32398b067f73c43361abddd82fe8c9
test <jessepaleg@outlook.com>
2024-05-18 14:32:58 +1000

    bump version

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -9,8 +9,8 @@ export const config = {
     repository_url: "https://github.com/xdnw/locutus",
     wiki_url: "https://github.com/xdnw/locutus/wiki",
     version: {
-        conflicts: 1.1,
-        conflict_data: 1.1,
-        graph_data: 1.1,
+        conflicts: 1.2,
+        conflict_data: 1.2,
+        graph_data: 1.2,
     }
 }
