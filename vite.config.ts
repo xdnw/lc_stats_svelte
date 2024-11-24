c5a5a85ebb582ba0a47e12e2f035f57f7836c2ca
test <jessepaleg@outlook.com>
2024-11-25 05:45:43 +1100

    Revert "Switch to ecma2015"
    This reverts commit 30b41ab9eb1e8a9eae16f17e1e68ccf72784fc82.

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,21 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import legacy from '@vitejs/plugin-legacy';
 
 export default defineConfig({
-    plugins: [
-        sveltekit(),
-        legacy({
-            targets: ['defaults', 'not IE 11']
-        })
-    ],
-    optimizeDeps: {
-        esbuildOptions: {
-            target: 'es2015'
-        }
-    },
-    build: {
-        minify: true,
-        target: 'es2015'
-    },
-    esbuild: {
-        target: 'es2015'
-    },
-    server: {
-        hmr: true
-    },
+	plugins: [sveltekit()],
+	optimizeDeps: {
+		esbuildOptions: {
+		  target: 'es2020'
+		}
+	  },
+	  build: {
+		minify: true,
+	  },
+	  esbuild: {
+		target: 'es2020',
+	  },
+	  server: {
+		hmr: true,
+	  },
 });
+
